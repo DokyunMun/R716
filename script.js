9d7a88e94cdf0e5d6aa364684eec52f01e50a74d
DokyunMun <85930592+DokyunMun@users.noreply.github.com>
2022-12-05 18:47:35 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById('a').onclick = function() {document.getElementById('A').scrollIntoView({behavior:"smooth"});};
+document.getElementById('b').onclick = function() {document.getElementById('B').scrollIntoView({behavior:"smooth"});};
+document.getElementById('c').onclick = function() {document.getElementById('C').scrollIntoView({behavior:"smooth"});};
+document.getElementById('d').onclick = function() {document.getElementById('D').scrollIntoView({behavior:"smooth"});};
+document.getElementById('e').onclick = function() {document.getElementById('E').scrollIntoView({behavior:"smooth"});};
+document.getElementById('f').onclick = function() {document.getElementById('F').scrollIntoView({behavior:"smooth"});};
+document.getElementById('g').onclick = function() {document.getElementById('G').scrollIntoView({behavior:"smooth"});};
+document.getElementById('h').onclick = function() {document.getElementById('H').scrollIntoView({behavior:"smooth"});};
+document.getElementById('i').onclick = function() {document.getElementById('I').scrollIntoView({behavior:"smooth"});};
+document.getElementById('j').onclick = function() {document.getElementById('J').scrollIntoView({behavior:"smooth"});};
+document.getElementById('k').onclick = function() {document.getElementById('K').scrollIntoView({behavior:"smooth"});};
+document.getElementById('l').onclick = function() {document.getElementById('L').scrollIntoView({behavior:"smooth"});};
+document.getElementById('m').onclick = function() {document.getElementById('M').scrollIntoView({behavior:"smooth"});};
+document.getElementById('n').onclick = function() {document.getElementById('N').scrollIntoView({behavior:"smooth"});};
+document.getElementById('o').onclick = function() {document.getElementById('O').scrollIntoView({behavior:"smooth"});};
+document.getElementById('p').onclick = function() {document.getElementById('P').scrollIntoView({behavior:"smooth"});};
+document.getElementById('q').onclick = function() {document.getElementById('Q').scrollIntoView({behavior:"smooth"});};
+document.getElementById('r').onclick = function() {document.getElementById('R').scrollIntoView({behavior:"smooth"});};
+document.getElementById('s').onclick = function() {document.getElementById('S').scrollIntoView({behavior:"smooth"});};
+document.getElementById('t').onclick = function() {document.getElementById('T').scrollIntoView({behavior:"smooth"});};
+document.getElementById('u').onclick = function() {document.getElementById('U').scrollIntoView({behavior:"smooth"});};
+document.getElementById('v').onclick = function() {document.getElementById('V').scrollIntoView({behavior:"smooth"});};
+document.getElementById('w').onclick = function() {document.getElementById('W').scrollIntoView({behavior:"smooth"});};
+
+
+
+colors = [ 'orange', 'blue', 'pink', 'gray' ]
+const randomColor = colors[Math.floor(Math.random() * colors.length)];
+document.querySelector('html').style.color = randomColor;
